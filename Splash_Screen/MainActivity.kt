13bc50f67fcd53697094874f9b38a803327fdfee
Dipanshu Sharma <148381788+sharmaansh0611@users.noreply.github.com>
2024-10-23 17:19:30 +0000

    SignUp and Splash Screen

---
+++ Splash_Screen/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.dipanshusharmaapp.vampnotes
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignUp::class.java))
+            finish()
+        }, 1500)
+    }
+}
